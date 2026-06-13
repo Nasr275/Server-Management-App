576b8ce26e891c3683d9ea74bc4f2ab293874c4b
Abdelrahman Nasr <abdelrhmannasr275@gmail.com>
2026-06-13 10:40:14 +0300

    Utilized host elements for proper styling and leaner DOM

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,11 +1,15 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None, //disables style scoping this means that the control styles.css are now global
+  host: {
+    class: "control"  //instead of adding the control class to every app-control selector we can add it here where it will always be used automatically
+  }
 })
 export class ControlComponent {
   label = input.required<string>()
