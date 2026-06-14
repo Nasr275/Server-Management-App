0d686b4b2abf49eb122b0b4eaa43d4f8f7ea453a
Abdelrahman Nasr <abdelrhmannasr275@gmail.com>
2026-06-14 19:26:13 +0300

    applying dynamic class binding and literal types to current status variable

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -8,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './server-status.component.css'
 })
 export class ServerStatusComponent {
-  currentStatus = 'online'; 
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  //This is a typescript feature called literal types
+
+
+  constructor() {
+    setInterval(() => {
+      const rand = Math.random()
+
+      if (rand < 0.5) {
+        this.currentStatus = 'online'
+      }
+      else if (rand < 0.9) {
+        this.currentStatus = 'offline'
+      }
+      else {
+        this.currentStatus = 'unknown'
+      }
+    }, 5000)  //execute this function every 5 secs
+  }
 }
